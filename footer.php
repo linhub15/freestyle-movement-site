43278e9751fa1ba0f95878819a774e772066c801
Hubert <linhub15@gmail.com>
2016-07-18 19:57:20 -0600

    trying out php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+        <div class="container">
+            <hr />
+            <!-- Sponsors and Affliates -->
+            <div class="col-md-3">
+                <h4>Sponsors and Affiliates</h4>
+                <a href="http://www.dancecodestudio.ca/"><img src="images/dancecode_logo.jpg" alt="dance code" width="50" height="50" /></a>
+            </div>
+            <!-- Contact Information -->
+            <div class="col-md-3 contactFooter" >
+
+                <h4>Contact Us</h4>
+                     
+                <p><span class="glyphicon glyphicon-envelope"></span> YEG.FreestyleMovement@gmail.com </p>
+            </div>           
+        </div>        
+    </footer>
+</body>
+</html>
---
+++ navbar-footer/navbar.php
@@ -0,0 +1,41 @@
+<nav id="home-navbar" class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container container-fluid">
+        <!-- Brand and collapse toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+        <a class="navbar-brand" href="index.html"><img src="images/stamp_white.png" alt="Freestyle Movement Text"></a>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#topNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div><!-- /.navbar-header -->
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="topNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="index.html">Home</a></li>                       
+                <li class="dropdown">
+                	<a  href="about-mission.html" data-toggle="dropdown" class="dropdown-toggle">About<b class="caret"></b></a>
+                	<ul class="dropdown-menu">
+                		<li><a href="about-mission.html">Mission</a></li> 
+                		<li class="divider"></li>
+                		<li><a href="about-team.html">Team</a></li>
+                	</ul>
+                </li>                        
+                <li><a href="schedule.html">Schedule</a></li>                       
+                <li class="dropdown">
+                	<a href="classes.html" data-toggle="dropdown" class="dropdown-toggle">Classes<b class="caret"></b></a>
+                	<ul class="dropdown-menu">
+                		<li><a href="classes.html">Info</a></li>
+                		<li class="divider"></li>
+                		<li><a href="classes-level-one.html">Level 1</a></li>
+                        <li class="divider"></li>
+                		<li><a href="foundation-expansion/classes-link.html">Level 2</a></li>
+                        <!-- <li class="divider"></li>
+                		<li><a href="#">Level 3</a></li>
+                        <li class="divider"></li>
+                		<li><a href="#">Level 4</a></li>-->
+                    </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+    <div class="greenNav"></div> <!--green bar-->
+</nav>
