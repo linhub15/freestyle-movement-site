35423f12ff74116018cd5694149cc22d863cbbbd
Hubert Lin <linhub15@gmail.com>
2016-07-31 14:57:26 -0600

    index.php created and working

--- header.php
+++ header.php
@@ -17,13 +17,17 @@
     </script>
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- Site Style Sheet -->
     <link href="css/site.css" rel="stylesheet">
     <link href="css/classes-page.css" rel="stylesheet">
-<!-- JQuery Load before page-->
+    
+	<!-- JQuery Load before page-->
     <script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+    
     <!--Bootstrap JS-->
     <script src="js/bootstrap.js"></script>
+    
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
@@ -42,46 +46,63 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    
 </head>
-<body class="">
+<body>
     <header>
           <nav class="navbar navbar-inverse navbar-fixed-top">
+          
             <div class="container container-fluid">
+            
                 <!-- Brand and collapse toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-                <a class="navbar-brand" href="index.html">
-                    <img src="images/stamp_white.png" alt="Freestyle Movement Text">
-                </a>
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#topNavBar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                <!--<a class="navbar-brand" href="#">Freestyle Movement</a>
-                -->
+                
+                	<a class="navbar-brand" href="index.html">
+                    	<img src="images/stamp_white.png" alt="Freestyle Movement Text">
+                	</a>
+                	
+                	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#topNavBar">
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+                  	</button>
+                	<!-- <a class="navbar-brand" href="#">Freestyle Movement</a> -->
+                	
                 </div><!-- /.navbar-header -->
                 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="topNavBar">
+                
                     <ul class="nav navbar-nav">
-                        <li><a href="index.html">Home</a></li>                       
+                        <li>
+                        	<a href="index.html">Home</a>
+                        </li>                       
                         <li class="dropdown">
+                        
                             <a  href="about-mission.html" data-toggle="dropdown" class="dropdown-toggle">About<b class="caret"></b></a>
+                            
                             <ul class="dropdown-menu">
                                 <li><a href="about-mission.html">Mission</a></li> 
                                 <li class="divider"></li>
                                 <li><a href="about-team.html">Team</a></li>
                             </ul>
-                        </li>                        
-                        <li><a href="schedule.html">Schedule</a></li>                       
+                            
+                        </li>
+                        
+                        <li><a href="schedule.html">Schedule</a></li>           
+                                    
                         <li class="dropdown">
+                        
                             <a href="classes.html" data-toggle="dropdown" class="dropdown-toggle">Classes<b class="caret"></b></a>
+                            
                             <ul class="dropdown-menu">
+                            
                                 <li><a href="classes.html">Overview</a></li>
                                 <li class="divider"></li>
                                 <li><a href="classes-level-one.html">Level 1</a></li>
                                 <li class="divider"></li>
                                 <li><a href="foundation-expansion/classes-link.html">Level 2</a></li>
+                                
                                 <!-- <li class="divider"></li>
                                 <li><a href="#">Level 3</a>
                                 <li class="divider"></li>
@@ -90,6 +111,7 @@
                         </li>
                     </ul>
                 </div><!-- /.navbar-collapse -->
+                
             </div><!-- /.container-fluid -->
             <div class="greenNav"></div> <!--green bar-->
         </nav>
--- index.php
+++ index.php
@@ -1,3 +1,100 @@
-<?php phpinfo(); ?>
+<?php include 'header.php'; ?>
+<!-- Page Banner Image -->
+<img id="banner-image" class="index-banner" src="images/Banner.png">
+<!-- Start of Content -->
+	<div class="container">        
+		<div>        	
+        	<div class="col-md-8">
+        	<!--Welcome Text section-->
+        		<div class="welcome-section">
+	        		<h1>Welcome to Freestyle Movement</h1>
+	        		<p>Welcome to Freestyle Movement, we are Edmonton's drop-in breaking a.k.a breakdance location. We provide bboys and bgirls with a place to practice every Friday night from 7:00 - 10:30. We also offer drop-in &amp; registered classes. As a non-profit organization, we aim to provide three main services to the breaking community: teaching dance, hosting events, and providing a safe and fun place for dancers to hang-out &amp; practice.</p>
+
+					<p>We hope to educate people about the breaking art form, and Hiphop culture so that we can build an appreciation for our dance and strengthen the local dance community.</p>
+        		</div><!--End .welcome-section-->
+        	</div> <!--End .col-md-8 div-->
+
+        	 <!-- Social Media Section -->
+            <div class="col-md-4">            	
+            	<!-- <h1>Social Media</h1>
+            	<p>Follow us on Facebook, Twitter, Youtube and Instagram!</p> -->
+            	<div class="social-media-container">
+	            	<div class="social-media-icons">
+		            	<a href="https://www.facebook.com/freemovedanceco" target="_blank">
+		            		<img src="images/facebook_icon.png" alt="Facebook"/>
+		            	</a>
+		            	
+                        <!-- <a href="https://twitter.com/yeg_fs" target="_blank">
+	            		<img src="images/twitter_icon.png" alt="Twitter" />
+	            	</a> -->
+
+	            	<a href="https://www.youtube.com/channel/UC7LclvZuOxPbJhpUUhPOEVA" target="_blank">
+	            		<img src="images/youtube_icon.png" alt="Youtube" />
+	            	</a>
+	            	<a href="https://www.instagram.com/freemovedanceco/" target="_blank">
+	            		<img src="images/instagram_icon.png" alt="Instagram" />
+	            	</a>
+            	</div><!--End socialMediaIcons-->
+            	</div>
+
+                </div> <!-- End Social Media-->
+                <div class="col-md-12 overview-price-div">
+                <!--Announcement-->
+                <div class="col-md-6 overview-div">            
+                    <div class="overview-text-div">
+                        <h2><span class="glyphicon glyphicon-info-sign"></span> Overview</h2>
+                        <ul>
+                        
+                        
+                            <li>Drop-in is Cash Only</li>                           
+                            <li>Bboy Link's Foundation Expansion Registration<br/>
+                                <a class="btn btn-primary" href="http://goo.gl/forms/NUSOO4vV2TLglyBd2" target="_blank">Register</a>
+                            </li>
+                            <li>Waiver &amp; consent form<br/> 
+                                <a href="documents/Waiver_FOIP.pdf" class="btn btn-primary" target="_blank">Download Form</a>
+                            </li>           
+                            
+                            
+                        </ul>
+                    </div><!--End of overview div-->
+                </div>
+
+                <!-- Prices section-->
+                <div class="col-md-6 prices-div" >
+                        <h2>Prices</h2>
+                        <p>All drop-in dancers pay $2 cover at entrance.</p>
+                        <table class="table">
+                            <tr>
+                             <th class="col-md-8">Class</th>
+                             <th class="col-md-4">Cost</th>
+                            </tr>
+                            <tr>
+                                <td class="col-md-8">Drop-in Practice Session</td>
+                                <td class="col-md-4">Free</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-8">Drop-in Foundation Basics</td>
+                                <td class="col-md-4">Free</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-8">Registered Foundation Expansion (8 Weeks)</td>
+                                <td class="col-md-4">$120</td>
+                            </tr>
+                            <tr>
+                                <td class="col-md-8">Drop-in Breaker's Conditioning</td>
+                                <td class="col-md-4">TBA</td>
+                            </tr>
+                        </table>
+                </div>
+                </div><!--End of Announcement-price-div-->
+                <!-- <a class="twitter-timeline" href="https://twitter.com/YEG_FS">Tweets by YEG_FS</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script> -->
+
+                   	
+       
+           
+        </div>
+        
+    </div> <!-- End Content-->
 
 
+<?php include 'footer.php'; ?>
