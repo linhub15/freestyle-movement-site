feef52efa9bb1f63ff6dfa536416dc2e84f6b5d3
Hubert Lin <linhub15@gmail.com>
2018-09-24 15:18:22 -0600

    responsive nav

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+document.querySelector('.navbar-toggle')
+    .addEventListener('click', function() {
+        let navbarNav = document.querySelector('.navbar-collapse');
+        navbarNav.classList.toggle('show');
+});
