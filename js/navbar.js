a0e0d5d711c9e773794cbda230407d9c3928a8d0
Hubert Lin <linhub15@gmail.com>
2018-09-25 10:50:32 -0600

    tidy

--- js/navbar.js
+++ js/navbar.js
@@ -2,4 +2,6 @@ document.querySelector('.navbar-toggle')
     .addEventListener('click', function() {
         let navbarNav = document.querySelector('.navbar-collapse');
         navbarNav.classList.toggle('show');
+    // Change bars to X
 });
+
