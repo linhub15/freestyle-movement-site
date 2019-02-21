0125b4c0e3f97dd29867be1e2b44e0350400560f
Hubert Lin - Macbook <linhub15@gmail.com>
2019-02-21 01:38:26 -0700

    use bootstrap navbar

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.navbar-toggle')
-    .addEventListener('click', function() {
-        let navbarNav = document.querySelector('.navbar-collapse');
-        navbarNav.classList.toggle('show');
-    // Change bars to X
-});
-
