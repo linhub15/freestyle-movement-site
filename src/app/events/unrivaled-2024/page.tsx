c6be22b681fa593f86ae1ca94dfd871e19489c6a
Hubert Lin <linhub15@users.noreply.github.com>
2024-06-09 23:11:07 -0600

    remove unrivaled ticket links

--- src/app/events/unrivaled-2024/page.tsx
+++ src/app/events/unrivaled-2024/page.tsx
@@ -1,7 +1,5 @@
 import { Container } from "@/components/Container";
-import { Stripe } from "@/components/Stripe";
 import { Schedule } from "./schedule";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/16/solid";
 
 import Image from "next/image";
 
@@ -25,7 +23,7 @@ export default function Unrivaled2024() {
         </p>
         <p className="text-lg font-semibold">Saturday June 8, 2024</p>
         <p className="">
-          DJ Carry ON | MC Teio | MC Miracle
+          DJ Carry On | MC Teio | MC Miracle
         </p>
 
         <p>
@@ -36,31 +34,10 @@ export default function Unrivaled2024() {
         <ul className="list-disc px-4">
           <li>$200 - 1v1 Open Styles (Judged by AJ)</li>
           <li>$200 - 1v1 Footwork (Judged by Eazy)</li>
-          <li>$100 - 2v2 Youth Breaking (Judged by Eazy & Link?)</li>
+          <li>$100 - 2v2 Youth Breaking (Judged by Eazy)</li>
         </ul>
 
         <Schedule />
-
-        <div className="py-4">
-          <h3 className="font-semibold text-lg">Tickets</h3>
-          <p>
-            Spectator tickets are purchased through the Driven{" "}
-            <a
-              className="text-zinc-300 underline"
-              href="https://www.showpass.com/drivenedmonton2024/"
-              target="_blank"
-            >
-              Ticket Site{" "}
-              <ArrowTopRightOnSquareIcon className="w-4 h-4 inline" />
-            </a>
-          </p>
-          <div className="w-full">
-            <Stripe
-              pricingTableId="prctbl_1PDGFVJ1kSEluvIzlrItRLy6"
-              publishableKey="pk_live_51OJll2J1kSEluvIzvRDs6akU2WNo5RQ2xUNd5MQ92CAyA4J7CpOTdFm15g4jqoKPm9ip9hc9CR3KTXv0CBEVFa1I00VKExIAPZ"
-            />
-          </div>
-        </div>
 
         <div id="map">
           <a href="https://maps.app.goo.gl/o3LtfiF3NGUNh8Sa7" target="_blank">
